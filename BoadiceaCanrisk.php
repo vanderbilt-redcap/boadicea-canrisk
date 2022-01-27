576f83293905fba12769fa78670eecdd677e6761
Kyle McGuffin <kyle.mcguffin@vumc.org>
2022-01-27 11:31:59 -0500

    Pull data from R4 record to build the JSON string for CanRisk 1.0 Had to substitute History and FirstBirth as these are missing for now Get data to respond, logging to error log for now

--- BoadiceaCanrisk.php
+++ BoadiceaCanrisk.php
@@ -7,10 +7,217 @@ use ExternalModules\ExternalModules;
 class BoadiceaCanrisk extends AbstractExternalModule
 {
 	public function redcap_save_record( $project_id, $record, $instrument, $event_id, $group_id, $survey_hash, $response_id, $repeat_instance ) {
-	
+		$recordData = \REDCap::getData([
+			"project_id" => $project_id,
+			"records" => $record,
+			"return_format" => "json"
+		]);
+		error_log("Starting BOADICEA");
+		$recordData = json_decode($recordData,true);
+		$dob = false;
+		$menarche = false;
+		$parity = false;
+		$firstBirth = false;
+		$ocUse = false;
+		$mhtUse = false;
+		$weight = false;
+		$bmi = false;
+		$alcohol = false;
+		$height = false;
+		$history = false;
+		
+		foreach($recordData as $thisEvent) {
+			if($thisEvent["date_of_birth"] != "") {
+				$dob = $thisEvent["date_of_birth"];
+			}
+			if($thisEvent["age_first_period"] != "") {
+				$menarche = $thisEvent["age_first_period"];
+			}
+			if($thisEvent["how_many_children_do_you_h"] != "") {
+				$parity = $thisEvent["how_many_children_do_you_h"];
+			}
+			if($thisEvent["date_of_birth_child"] != "") {
+				if($dob !== false) {
+					$firstBirth = floor(datediff($dob,$thisEvent["date_of_birth_child"],"y"));
+				}
+			}
+			if($thisEvent["taken_oral_contraceptive_pill"] != "") {
+				if($thisEvent["taken_oral_contraceptive_pill"] == "2") {
+					$ocUse = "N";
+				}
+				else if($thisEvent["taken_oral_contraceptive_pill"] == "1") {
+					if($thisEvent["pill_taken_last_two_years"] == "1") {
+						$ocUse = "C";
+					}
+					else if($thisEvent["pill_taken_last_two_years"] == "0") {
+						$ocUse = "F";
+					}
+					switch($thisEvent["how_many_years_have_you_ta"]) {
+						case 1:
+							$ocUse .= "1";
+							break;
+						case 2:
+							$ocUse .= "3";
+							break;
+						case 3:
+							$ocUse .= "7";
+							break;
+						case 4:
+							$ocUse .= "13";
+							break;
+						case 5:
+							$ocUse .= "15";
+							break;
+					}
+				}
+			}
+			if($thisEvent["hrt_for_menopause"] != "") {
+				switch($thisEvent["hrt_for_menopause"]) {
+					case 1:
+						$mhtUse = "N";
+						break;
+					case 2:
+						$mhtUse = "F";
+						break;
+					case 3:
+					case 4:
+						if($thisEvent["type_of_hrt"] == "1") {
+							$mhtUse = "E";
+						}
+						else {
+							$mhtUse = "C";
+						}
+						break;
+				}
+			}
+			if($thisEvent["current_weight"] != "") {
+				$weight = $thisEvent["current_weight"];
+				$weight /= 2.2;
+			}
+			if($thisEvent["height_feet"] != "") {
+				$height = $thisEvent["height_feet"] * 12;
+				$height += (int)$thisEvent["height_inches"];
+				$height *= 0.0254;
+			}
+			
+			if($height !== false && $weight !== false) {
+				$bmi = round($weight / $height / $height * 10) / 10;
+				$height = round($height * 100);
+			}
+			
+			if($thisEvent["drink_containing_alcohol"] != "") {
+				switch($thisEvent["drink_containing_alcohol"]) {
+					case 1:
+						$freq = 0;
+						$alcohol = "0";
+						break;
+					case 6:
+						$alcohol = "NA";
+						break;
+					case 2:
+						$freq = 1/30;
+						break;
+					case 3:
+						$freq = 3/30;
+						break;
+					case 4:
+						$freq = 2.5/7;
+						break;
+					case 5:
+						$freq = 4/7;
+						break;
+				}
+				
+				if($alcohol != "NA" && $alcohol !== "0") {
+					switch($thisEvent["how_many_drinks_do_you_have"]) {
+						case 1:
+							$alcohol = "0";
+							break;
+						case 2:
+							$alcohol = $freq * 1.5;
+							break;
+						case 3:
+							$alcohol = $freq * 3.5;
+							break;
+						case 4:
+							$alcohol = $freq * 5.5;
+							break;
+						case 5:
+							$alcohol = $freq * 7.5;
+							break;
+						case 6:
+							$alcohol = $freq * 10;
+							break;
+						case 7:
+							$alcohol = "NA";
+							break;
+					}
+				}
+				
+				if($alcohol != "NA" && $alcohol != "0") {
+					$alcohol = round($alcohol * 10) / 10;
+				}
+			}
+		}
+		## Temp data section since some things are broken/missing on survey
+		$firstBirth = 25;
+		$history = "FamID	Name	Target	IndivID	FathID	MothID	Sex	MZtwin	Dead	Age	Yob	BC1	BC2	OC	PRO	PAN	Ashkn	BRCA1	BRCA2	PALB2	ATM	CHEK2	RAD51D	RAD51C	BRIP1	ER:PR:HER2:CK14:CK56
+XXXX	pa	0	m21	0	0	M	0	0	0	0	0	0	0	0	0	0	0:0	0:0	0:0	0:0	0:0	0:0	0:0	0:0	0:0:0:0:0
+XXXX	ma	0	f21	0	0	F	0	0	0	0	0	0	0	0	0	0	0:0	0:0	0:0	0:0	0:0	0:0	0:0	0:0	0:0:0:0:0
+XXXX	me	1	ch1	m21	f21	F	0	0	35 1986	0	0	0	0	0	0	S:N	S:N	S:N	S:N	0:0	0:0	0:0	0:0	0:0:0:0:0";
+		
+		$dataString = $this->compressRecordData($dob, $menarche, $parity, $firstBirth, $ocUse,
+												$mhtUse, $weight, $bmi, $alcohol, $height,
+												$history);
+		error_log("Have Data String\n".$dataString);
+		if($dataString !== false) {
+			$response = $this->sendRequest($dataString);
+			error_log(var_export($response,true));
+		}
+		else {
+			error_log("Failed to send");
+		}
+		
+		## Do something to save the response to the record
 	}
 	
-	public function sendRequest($data) {
+	public function compressRecordData($dob, $menarche, $parity, $firstBirth, $ocUse,
+									   $mhtUse, $weight, $bmi, $alcohol, $height,
+										$history) {
+		if($dob === false || $menarche === false || $parity === false || $weight === false ||
+				$height === false || $alcohol === false || $history === false) {
+			return false;
+		}
+		
+		$dataString = "##CanRisk 1.0\n".
+			"##menarch=$menarche\n".
+			"##BMI=$bmi\n".
+			"##alcohol=$alcohol\n".
+			"##height=$height\n".
+			"##parity=$parity\n";
+		
+		if($firstBirth) {
+			$dataString .= "##First_live_birth=".$firstBirth."\n";
+		}
+		if($ocUse) {
+			$dataString .= "##OC_use=".$ocUse."\n";
+		}
+		if($mhtUse) {
+			$dataString .= "##MHT_use".$mhtUse."\n";
+		}
+		
+		$dataString .= "##".$history."\n";
+		
+		return $dataString;
+	}
+	
+	public function sendRequest($pegigreeData) {
+		$pegigreeData = str_replace("\r\n","\\n",$pegigreeData);
+		$pegigreeData = str_replace("\n","\\n",$pegigreeData);
+		$pegigreeData = str_replace("\t","\\t",$pegigreeData);
+		
+		$data = '{"mut_freq":"UK","cancer_rates":"UK","user_id":"mcguffk","pedigree_data":"'.$pegigreeData.'"}';
+		
 		$apiUrl = $this->getProjectSetting("api-url");
 		$apiToken = $this->getProjectSetting("auth-token");
 		echo $apiToken."<br />";
--- test.php
+++ test.php
@@ -28,13 +28,9 @@ XXXX	15	0	Fxsy	m21	f21	M	0	0	0	0	0	0	0	0	0	0	0:0	0:0	0:0	0:0	0:0	0:0	0:0	0:0	0:0
 XXXX	16	0	jXjW	ySRJ	ch1	F	0	0	22	1999	0	0	0	0	0	0	0:0	0:0	0:0	0:0	0:0	0:0	0:0	0:0	0:0:0:0:0
 XXXX	17	0	gDbD	ySRJ	ch1	M	0	0	28	1993	0	0	0	0	0	0	0:0	0:0	0:0	0:0	0:0	0:0	0:0	0:0	0:0:0:0:0";
 
-$pedigreeData = str_replace("\r\n","\\n",$pedigreeData);
-$pedigreeData = str_replace("\t","\\t",$pedigreeData);
-
-$data = '{"mut_freq":"UK","cancer_rates":"UK","user_id":"mcguffk","pedigree_data":"'.$pedigreeData.'"}';
 //$data = '{"mut_freq":"UK","cancer_rates":"UK","user_id":"mcguffk","pedigree_data":"##CanRisk 1.0\n##menarche=12\n##oc_use=N\n##mht_use=N\n##BMI=23.01\n##alcohol=44.8\n##height=180.34\n##FamID\tName\tTarget\tIndivID\tFathID\tMothID\tSex\tMZtwin\tDead\tAge\tYob\tBC1\tBC2\tOC\tPRO\tPAN\tAshkn\tBRCA1\tBRCA2\tPALB2\tATM\tCHEK2\tRAD51D\tRAD51C\tBRIP1\tER:PR:HER2:CK14:CK56\nXXXX\tpa\t0\tm21\t0\t0\tM\t0\t0\t0\t0\t0\t0\t0\t0\t0\t0\t0:0\t0:0\t0:0\t0:0\t0:0\t0:0\t0:0\t0:0\t0:0:0:0:0\nXXXX\tma\t0\tf21\t0\t0\tF\t0\t0\t0\t0\t0\t0\t0\t0\t0\t0\t0:0\t0:0\t0:0\t0:0\t0:0\t0:0\t0:0\t0:0\t0:0:0:0:0\nXXXX\tme\t1\tch1\tm21\tf21\tF\t0\t0\t42\t1977\t38\t0\t0\t0\t0\t0\tS:N\tS:N\tS:N\tS:N\t0:0\t0:0\t0:0\t0:0\t0:0:0:0:0\n"}';
 
-$output = $module->sendRequest($data);
+$output = $module->sendRequest($pedigreeData);
 
 echo "<br /><pre>";
 var_dump($output);
